3c9fafb8fd06afb33a494d0c8159365770a9a267
Wil999 <wil1999_@hotmail.com>
2024-05-16 12:16:38 -0500

    commit initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CarouselComponent } from './carousel/carousel.component';
+import { ICarouselItem } from './carousel/carousel-item.metadata';
+import { CAROUSEL_DATA_ITEMS } from './carousel/carousel.const';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CarouselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  public carouselData: ICarouselItem[] = CAROUSEL_DATA_ITEMS;
   title = 'carousel';
 }
---
+++ src/app/carousel/carousel.const.ts
@@ -0,0 +1,34 @@
+import { ICarouselItem } from './carousel-item.metadata';
+
+export const CAROUSEL_DATA_ITEMS: ICarouselItem[]=[
+  {
+    id:1,
+    title:{
+      first:'TITULO',
+      second:'Principal'
+    },
+    subtitle:'Esto es una descripción',
+    link:'/',
+    image:'assets/carousel-images/1.jpg'
+  },
+  {
+    id:2,
+    title:{
+      first:'TITULO',
+      second:'Segundo'
+    },
+    subtitle:'Esto es una descripción',
+    link:'/',
+    image:'assets/carousel-images/2.jpg'
+  },
+  {
+    id:3,
+    title:{
+      first:'TITULO',
+      second:'Tercero'
+    },
+    subtitle:'Esto es una descripción',
+    link:'/',
+    image:'assets/carousel-images/3.jpg'
+  }
+];
